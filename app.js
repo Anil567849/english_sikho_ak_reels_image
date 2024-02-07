0a3c4cc461e0d64e1f8495b619c8a67e0f7944a7
Anil <anilkumar567849@gmail.com>
2024-02-07 14:47:26 +0530

    add single phrase on text's code

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
 import generateName from './utils/generateName.js';
 import putPhraseOnPhoto from './utils/putPhraseOnPhoto.js';
 import putVocabOnPhoto from './utils/putVocabOnPhoto.js';
+import putSinglePhraseOnPhoto from './utils/putSinglePhraseOnPhoto.js';
 import currentDate from './utils/createDate.js';
 
 const data = [
     {
-        englishText: "Professionally",
-        hindiText: "व्यावसायिक",
-    }
-    // {
-    //     englishText: "I need to deposit some money into my account.",
-    //     hindiText: "मुझे अपने खाते में कुछ पैसे जमा करने हैं।",
-    // }
-    // {
-    //     englishText: "What are the current interest rates for savings",
-    //     hindiText: "बचत खातों के लिए वर्तमान ब्याज दरें क्या हैं?",
-    // }
+        hindiText: "असंभव, मेरा शब्द नहीं!",
+        englishText: "Impossible isn't my vocabulary!",
+    },
+    {
+        hindiText: "आज इतिहास रचेंगे!",
+        englishText: "Making history today!",
+    },
+    {
+        hindiText: "हर चुनौती में छुपा है जीत का रास्ता!",
+        englishText: "Victory hidden in every challenge!",
+    },
+    {
+        hindiText: "मुझे कोई नहीं रोक सकता!",
+        englishText: "Nobody can stop me!",
+    },
 ]
 
 function useManualUrl(height, width, urls, folderName, type){
   const time = currentDate();
 
-  for(let i = 0; i < 1; i++){
+  for(let i = 0; i < data.length; i++){
     const {englishText, hindiText} = data[i];
-    // console.log(text);
+    // console.log(englishText, hindiText);
     const photoPath = `${urls}${i+1}.jpg`;
-    // const photoPath = urls[i];
-    // console.log(photoPath);
-    const fileName = generateName();
-    const outputPath = (`E:/Instagram/${folderName}/${time}/${fileName}.jpg`);
 
+    // const fileName = generateName(); // random name generator
+    const fileName = "0" + (i + 1).toString();
+    const outputPath = (`E:/Instagram/${folderName}/${time}/${fileName}.jpg`);
+    
     if(type == 'phrase'){
         putPhraseOnPhoto(photoPath, englishText, hindiText, outputPath, height, width);
-    }else{
+    }else if(type == 'vocab'){
         putVocabOnPhoto(photoPath, englishText, hindiText, outputPath, height, width);
+    }else{
+        putSinglePhraseOnPhoto(photoPath, englishText, hindiText, outputPath, height, width);
     }
   }
 }
@@ -44,8 +51,9 @@ function useManualUrl(height, width, urls, folderName, type){
         const width = 1080;
         const lang = "hindi";
 
-        useManualUrl(height, width, './utils/imagesData/local-images/', "English Sikho AK", "vocab");
-        // useManualUrl(height, width, './utils/imagesData/local-images/', "English Sikho AK", "phrase");
+        // useManualUrl(height, width, './utils/imagesData/local-images/', "English Sikho AK", "vocab");
+        useManualUrl(height, width, './utils/imagesData/local-images/', "English Sikho AK", "phrase");
+        // useManualUrl(height, width, './utils/imagesData/local-images/', "English Sikho AK", "single-phrase"); // put your content under englishText obj and leave hindiText blank
 
     } catch (error) {
         console.log("app.js", error);
